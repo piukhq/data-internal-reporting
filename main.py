936f3da00cc62ac70db3485194e62d0bd464e7b0
Tehmoor Ul Haq <tulhaq@bink.com>
2022-03-11 17:24:50 +0000

    Added Bink Barclays Users and colour coding

--- main.py
+++ main.py
@@ -16,26 +16,32 @@ def home():
         SELECT
             date_joined::DATE,
             name,
+            user_id IS NULL consent,
             COUNT(DISTINCT \"user\".id) users
         FROM
             \"user\"
         INNER JOIN
             user_clientapplication ON \"user\".client_id = user_clientapplication.client_id
+        LEFT JOIN
+            ubiquity_serviceconsent ON \"user\".id = ubiquity_serviceconsent.user_id
         WHERE
-            date_joined > (CURRENT_DATE - interval '60 days') AND
-            date_joined < CURRENT_DATE
+            name = \'Bink\' OR name = \'Barclays Mobile Banking\'
         GROUP BY
             date_joined::DATE,
-            name
-    """
+            name,
+            consent
+"""
+
     df = pd.read_sql(query,conn)
-    df = df.pivot(index='date_joined',columns='name',values='users').fillna(0)[['Bink','Barclays Mobile Banking']].reset_index()
-    # day = (datetime.today() - timedelta(days=60)).strftime('%Y/%m/%d')
-    # df = df[pd.to_datetime(df['date_joined']) >= day]
+    bink_users = df.groupby(df['name']).agg({'users':'sum'})['users']['Bink']
+    barclays_users = df.groupby(df['name']).agg({'users':'sum'})['users']['Barclays Mobile Banking']
+    df = df.drop('consent',axis=1).groupby(['date_joined','name']).agg({'users':'sum'}).reset_index().pivot(index='date_joined',columns='name',values='users').fillna(0).reset_index()
+    day = (datetime.today() - timedelta(days=60)).strftime('%Y/%m/%d')
+    df = df[pd.to_datetime(df['date_joined']) >= day]
     labels = list(set(df['date_joined'].astype(str).tolist()))
     bink = df['Bink'].astype(str).tolist()
     barclays = df['Barclays Mobile Banking'].astype(str).tolist()
-    return render_template('home.html', labels=labels, bink=bink, barclays=barclays)
+    return render_template('home.html', labels=labels,bink=bink,barclays=barclays,bink_users=bink_users,barclays_users=barclays_users)
 
 if __name__ == "__main__":
     app.run(debug=True)
