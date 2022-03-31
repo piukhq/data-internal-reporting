7514598803b63dc456a25890e3c40257c26ad91c
Anand Bhakta <abhakta@bink.com>
2022-03-31 14:32:20 +0100

    gitignore

--- supporting_files/env.py
+++
@@ -1,6 +0,0 @@
-hostref="bink-uksouth-tableau.postgres.database.azure.com"
-databasenameref="hermes"
-databasenamerefharm="harmonia"
-usernameref="dogfish"
-passwordref="BKlXaolxX1LfENcifzygI6WP"
-portref="5432"
