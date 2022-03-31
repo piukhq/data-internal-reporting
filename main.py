473d8aa2a2dca5aa628533768481585cd3dca3c4
Tehmoor Ul Haq <tulhaq@bink.com>
2022-03-31 10:57:20 +0100

    Added pie charts for demo

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def home():
             date_joined::DATE,
             name,
             consent
-"""
-
+    """
+    
     df = pd.read_sql(query,conn)
     bink_users = df.groupby(df['name']).agg({'users':'sum'})['users']['Bink']
     barclays_users = df.groupby(df['name']).agg({'users':'sum'})['users']['Barclays Mobile Banking']
@@ -42,7 +42,51 @@ def home():
     labels = df['date_joined'].astype(str).tolist()
     bink = df['Bink'].astype(str).tolist()
     barclays = df['Barclays Mobile Banking'].astype(str).tolist()
-    return render_template('home.html', labels=labels,bink=bink,barclays=barclays,bink_users=bink_users,barclays_users=barclays_users)
+
+    query = """
+        SELECT
+            user_clientapplication.name,
+            company,
+            jsonb_array_length(pll_links) <> 0 pll_link,
+            COUNT( DISTINCT card_number) cards
+        FROM
+            scheme_schemeaccount
+        INNER JOIN
+            scheme_scheme ON scheme_schemeaccount.scheme_id = scheme_scheme.id
+        INNER JOIN
+            ubiquity_schemeaccountentry ON scheme_schemeaccount.id = ubiquity_schemeaccountentry.scheme_account_id
+        INNER JOIN
+            \"user\" ON ubiquity_schemeaccountentry.user_id = \"user\".id
+        INNER JOIN
+            user_clientapplication ON \"user\".client_id = user_clientapplication.client_id
+        WHERE
+            (user_clientapplication.name = \'Bink\' OR user_clientapplication.name = \'Barclays Mobile Banking\') AND 
+            is_deleted = FALSE AND status = 1
+        GROUP BY
+            company,
+            user_clientapplication.name,
+            jsonb_array_length(pll_links) <> 0
+    """
+
+    df = pd.read_sql(query, conn)
+    comps = ['Harvey Nichols', 'Iceland', 'SquareMeal', 'Wasabi']
+    bink_lc = df[
+        (df['pll_link']==True) & (df['name']=='Bink') & (df['company'].isin(comps))
+        ][['company','cards']].rename(columns={'cards':'Bink'})
+    barc_lc = df[
+        (df['pll_link']==True) & (df['name']=='Barclays Mobile Banking') & (df['company'].isin(comps))
+        ][['company','cards']].rename(columns={'cards':'Barclays'})
+
+    lcs = pd.merge(bink_lc,barc_lc,'outer','company').fillna(0)
+    lcs['Barclays'] = lcs['Barclays'].astype(int)
+
+    lcs_label = lcs['company'].tolist()
+    lcs_bink = lcs['Bink'].tolist()
+    lcs_barc = lcs['Barclays'].tolist()
+
+    return render_template('home.html', 
+        labels=labels,bink=bink,barclays=barclays,bink_users=bink_users,barclays_users=barclays_users,
+        lcs_label=lcs_label,lcs_bink=lcs_bink,lcs_barc=lcs_barc)
 
 if __name__ == "__main__":
     app.run(debug=True)
